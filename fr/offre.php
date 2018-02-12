09a3d12121eba8680d01a18da995e53ad49a0f80
Zerby <clement.zerbi@eemi.com>
2018-02-12 17:55:11 +0100

    page offre

--- fr/offre.php
+++ fr/offre.php
@@ -4,10 +4,10 @@ $description = "écrire la meta description de la page";
 include('../include/header.php');
 ?>
 		<section class="blue h-85">
-			<div class="container flex-col-center-sa h-90">
-				<h2 class="text-white">Nos Offres</h2>
+			<div class="container flex-col-center-sa h-85">
+                <div class="bloc_bordered white flex-col-center padding">
 				<div class="flex-sb w-100">
-				<div class="w-40 flex-col-center white bloc_bordered padding">
+				<div class="w-50 flex-col-center">
 					<h3 class="text-blue">Débutant</h3>
 					<p class="margin-0">Vous ne connaissez pas la lsf</p>
 					<p class="margin-0">mais vous souhaitez apprendre les fondamentaux</p>
@@ -15,15 +15,50 @@ include('../include/header.php');
 					<a href="#" title="" class="button orange text-white w-50">Choisir cette offre</a>
 				</div>
 
-				<div class="w-40 flex-col-center white bloc_bordered padding">
-					<h3 class="text-blue">Intermédiaire</h3>
-					<p class="margin-0">Vous ne connaissez pas la lsf</p>
-					<p class="margin-0">mais vous souhaitez apprendre les fondamentaux</p>
-					<img src="../images/offres.png" class="w-90">
-					<a href="#" title="" class="button orange text-white w-50">Choisir cette offre</a>
+                    <div class="w-50 h-70 flex-col-sa">
+                        <h3>L’offre débutante qui vous permettra  :</h3>
+                        <p class="margin-0">- De travailler et d’apprendre la gestuelle</p>
+                        <p class="margin-0">- D’apprendre des mots simples</p>
+                        <p class="margin-0">- De construire des phrases</p>
+                        <p class="margin-0">- De comprendre une question</p>
+
+                        <p>L’offre débutante recouvre le <span class="text-orange bold">niveau A1</span> qui contient les mots principaux,
+                            la gestuelles ainsi que les verbes et le <span class="text-orange bold">niveau A2</span> qui contient des phrases
+                            (sujet/verbe/complément) et à comprendre des questions.</p>
+                    </div>
 				</div>
-				</div>
+                </div>
 			</div>
 		</section>
 
+<section class="blue h-85">
+    <div class="container flex-col-center-sa h-85">
+        <div class="bloc_bordered white flex-col-center padding">
+            <div class="flex-sb w-100">
+                <div class="w-50 flex-col-center">
+                    <h3 class="text-blue">Intermédiaire</h3>
+                    <p class="margin-0">Vous ne connaissez pas la lsf</p>
+                    <p class="margin-0">mais vous souhaitez apprendre les fondamentaux</p>
+                    <img src="../images/offres.png" class="w-90">
+                    <a href="#" title="" class="button orange text-white w-50">Choisir cette offre</a>
+                </div>
+
+                <div class="w-50 h-70 flex-col-sa">
+                    <h3>L’offre intermédiaire qui vous permettra :</h3>
+                    <p class="margin-0">- D’apprendre à décrire une situation</p>
+                    <p class="margin-0">- De répondre à des questions</p>
+                    <p class="margin-0">- De débattre</p>
+                    <p class="margin-0">- De communiquer dans un groupe</p>
+
+                    <p>L’offre intermédiaire recouvre le <span class="text-orange bold">niveau B1</span> qui contient la description d’une situation,
+                        et la réponse aux questions et le <span class="text-orange bold">niveau B2</span> qui permettra de débattre,
+                        ainsi que de communiquer dans un groupe.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <?php include('../include/footer.php') ?>
+
