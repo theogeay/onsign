a24517ed60474e33c0cac86aa9fb7609149e19e8
Zerby <clement.zerbi@eemi.com>
2018-02-19 14:50:51 +0100

    page offre en row en col ça ne rentre pas

--- fr/offre.php
+++ fr/offre.php
@@ -3,24 +3,26 @@ $title = "Offre";
 $description = "Description de la page offre";
 include('../include/header.php');
 ?>
-		<section class="blue h-85">
-			<div class="container flex-col-center-sa h-85">
+		<section class="blue padding">
+			<div class="container flex-col-center-sa">
                 <div class="bloc_bordered white flex-col-center padding">
-				<div class="flex-sb w-100">
-				<div class="w-50 flex-col-center">
+				<div class="flex-sb row">
+				<div class="col s12 m6 l6 flex-col-center">
 					<h3 class="text-blue">Débutant</h3>
 					<p class="margin-0">Vous ne connaissez pas la lsf</p>
 					<p class="margin-0">mais vous souhaitez apprendre les fondamentaux</p>
 					<img src="../images/offres.png" class="w-90">
-					<a href="#" title="" class="button orange text-white w-50">Choisir cette offre</a>
+                    <div class="row">
+                        <a href="#" title="" class="button orange text-white w-50 col s12">Choisir cette offre</a>
+                    </div>
 				</div>
 
-                    <div class="w-50 h-70 flex-col-sa">
+                    <div class="col s12 m6 l6 flex-col-sa">
                         <h3>L’offre débutante qui vous permettra  :</h3>
-                        <p class="margin-0">- De travailler et d’apprendre la gestuelle</p>
-                        <p class="margin-0">- D’apprendre des mots simples</p>
-                        <p class="margin-0">- De construire des phrases</p>
-                        <p class="margin-0">- De comprendre une question</p>
+                        <p>- De travailler et d’apprendre la gestuelle</p>
+                        <p>- D’apprendre des mots simples</p>
+                        <p>- De construire des phrases</p>
+                        <p>- De comprendre une question</p>
 
                         <p>L’offre débutante recouvre le <span class="text-orange bold">niveau A1</span> qui contient les mots principaux,
                             la gestuelles ainsi que les verbes et le <span class="text-orange bold">niveau A2</span> qui contient des phrases
@@ -31,24 +33,27 @@ include('../include/header.php');
 			</div>
 		</section>
 
-<section class="blue h-85">
-    <div class="container flex-col-center-sa h-85">
+<section class="blue padding">
+    <div class="container flex-col-center-sa">
         <div class="bloc_bordered white flex-col-center padding">
-            <div class="flex-sb w-100">
-                <div class="w-50 flex-col-center">
+            <div class="flex-sb row">
+                <div class="col s12 m6 l6 flex-col-center">
                     <h3 class="text-blue">Intermédiaire</h3>
                     <p class="margin-0">Vous ne connaissez pas la lsf</p>
                     <p class="margin-0">mais vous souhaitez apprendre les fondamentaux</p>
                     <img src="../images/offres.png" class="w-90">
-                    <a href="#" title="" class="button orange text-white w-50">Choisir cette offre</a>
+                    <div class="row">
+                        <a href="#" title="" class="button orange text-white w-50 col s12">Choisir cette offre</a>
+                    </div>
+
                 </div>
 
-                <div class="w-50 h-70 flex-col-sa">
+                <div class="col s12 m6 l6 flex-col-sa">
                     <h3>L’offre intermédiaire qui vous permettra :</h3>
-                    <p class="margin-0">- D’apprendre à décrire une situation</p>
-                    <p class="margin-0">- De répondre à des questions</p>
-                    <p class="margin-0">- De débattre</p>
-                    <p class="margin-0">- De communiquer dans un groupe</p>
+                    <p>- D’apprendre à décrire une situation</p>
+                    <p>- De répondre à des questions</p>
+                    <p>- De débattre</p>
+                    <p>- De communiquer dans un groupe</p>
 
                     <p>L’offre intermédiaire recouvre le <span class="text-orange bold">niveau B1</span> qui contient la description d’une situation,
                         et la réponse aux questions et le <span class="text-orange bold">niveau B2</span> qui permettra de débattre,
