4f5dcda326030c0521b965859d52fde885750fe9
Zerby <clement.zerbi@eemi.com>
2018-03-09 22:28:53 +0100

    profil.php

--- fr/profil.php
+++ fr/profil.php
@@ -2,64 +2,126 @@
 $title = "Votre profil";
 $description = "écrire la meta description de la page";
 $main_color = "white";
-include('../include/header.php');
-
+session_start();
 //connexion BDD 
 require_once('../admin/connect.php');
-// affichage table 
-$profil = $pdo->query('SELECT * FROM users'); 
-$profils = $profil->fetch(PDO::FETCH_ASSOC);
+//affchage								
+$sql = "SELECT * FROM `users` WHERE id = :id";
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', $_SESSION['id']);
+$stmt->execute();
+$value = $stmt -> fetch();
+if(isset($_POST['envoyer'])){
+	
+	if(!isset($_GET['id'])){
+    die('L\'id pas spécifiée!');
+}
+	
+	//modification
+$modif = "UPDATE users SET 
+nom = :nom, 
+prenom = :prenom, 
+mail = :mail, 
+adresse = :adresse, 
+telephone = :telephone, 
+mdp = :mdp, 
+pseudo = :pseudo, 
+photo = :photo 
+WHERE id = :id";
+    $stmt2 = $pdo->prepare($modif);
+  	$stmt2->bindValue(':id', $_SESSION['id']);
+    $stmt2->bindValue(':prenom', $_POST['prenom']);
+    $stmt2->bindValue(':nom', $_POST['nom']);
+    $stmt2->bindValue(':mail', $_POST['mail']);
+    $stmt2->bindValue(':adresse', $_POST['adresse']);
+    $stmt2->bindValue(':telephone', $_POST['telephone']);
+    $stmt2->bindValue(':mdp', $_POST['mdp']);
+    $stmt2->bindValue(':pseudo', $_POST['pseudo']);
+    $stmt2->bindValue(':photo', $_POST['photo']);
+    var_dump($stmt2->execute());
+//    var_dump($stmt2->errorInfo());
+    header('Location: profil.php');
+    die();
+
+}
 
 
+// modif image 
+
+
+
+ if(!empty($_POST['submit']) && isset($_FILES['avatar']))
+     {
+         
+         $destination = "../images/photo_profil/";
+         
+         $filename = basename($_FILES['avatar']['name']);
+
+         
+         move_uploaded_file($_FILES['avatar']['tmp_name'], $destination . $filename);
+//var_dump($_FILES['avatar']['tmp_name']);
+  //      die();
+   }
+include('../include/header.php');
 ?>
 <section class="flex-col-center container">
     
         <div class="flex-col-center row m-top-30px col s12">
-					<h1 class="black-text">Votre profil</h1>
-								<div class="flex-start w-60">
-									<div ><img class="photoprofil" alt="photo de profil Onsign" src="../images/exemple.jpg">
-									<a href="#">Modifier</a></div>
-		</div>
+					<h1>Votre profil</h1>
+								
 	
         </div>
-	
 
-			<form method="post" action="" class="w-60 flex-sa m-bot-30px row ">
-            <div class="flex-col-center col s12 l6 m6"><div class="hidden">
+<div class="flex-start w-60">
+    <div>
+        <img class="photoprofil" name="photo" alt="photo de profil Onsign" src="<?='../images/photo_profil/' . $value['photo'] ?>">
+        <form enctype="multipart/form-data" action="profil.php?id=<?= $value['id'] ?>" method="post">
+            <input type="file" name="avatar" />
+            <input type="submit" value="upload" name="submit">
+        </form>
+    </div>
+		</div>
+			<form action="profil.php?id=<?= $value['id'] ?>" method="post" class="w-60 flex-sa m-bot-30px row ">
+					
+            <div class="flex-col-center col s12 l6 m6">
+                <div class="hidden">
                 <p>id</p>
-                <input class="browser-default" name="id"  >
+                <input value="<?=$value['id']?>" class="browser-default" name="id"  >
             </div>
+						
             <div class="flex-sb m-top-5px w-85">
                 <p>Nom</p>
                 <input value="<?=$value['nom']?>" class="browser-default"  name="nom"  >
             </div>
             <div class="flex-sb m-top-5px w-85">
                 <p>Prénom</p>
-                <input class="browser-default"  name="prenom">
+                <input class="browser-default"  name="prenom" value="<?=$value['prenom']?>" >
             </div>
             <div class="flex-sb m-top-5px w-85">
                 <p>Mail</p>
-                <input class="browser-default"  name="mail"  >
+                <input value="<?=$value['mail']?>"  class="browser-default"  name="mail"  >
             </div>
-            <div class="flex-sb m-top-5px w-85">
+						<div class="flex-sb m-top-5px w-85">
                 <p>Adresse</p>
-                <textarea name="adresse"  ></textarea>
+                <textarea    name="adresse"  ><?=$value['adresse']?></textarea>
             </div>
 							</div>
 					<div class="flex-col-center col s12 l6 m6">
+
             <div class="flex-sb m-top-5px w-85">
                 <p>Téléphone</p>
-                <input class="browser-default"  name="telephone"  >
+                <input value="<?=$value['telephone']?>"  class="browser-default"  name="telephone"  >
             </div>
             <div class="flex-sb m-top-5px w-85">
                 <p>Pseudo</p>
-                <input class="browser-default"  name="pseudo"  >
+                <input value="<?=$value['pseudo']?>"  class="browser-default"  name="pseudo"  >
             </div>
             <div class="flex-sb m-top-5px w-85">
                 <p>Mot de passe</p>
-                <input class="browser-default"  name="mdp"  >
+                <input value="<?=$value['mdp']?>"  class="browser-default"  name="mdp"  >
             </div>
-            <button class="button orange w-85 m-bot-10px">Enrgistrer les changements</button>
+            <button type="submit" name="envoyer" class="button orange w-85 m-bot-10px">Enrgistrer les changements</button>
 						</div>
         </form>
     
