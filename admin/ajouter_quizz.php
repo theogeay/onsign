084abf560e07ab6e2c98d7b69844bff686e6c772
Zerby <clement.zerbi@eemi.com>
2018-03-11 23:18:21 +0100

    quizz

--- admin/ajouter_quizz.php
+++ admin/ajouter_quizz.php
@@ -11,12 +11,62 @@ $main_color = "white";// background_color du main
 $titre = "Quizz";
 require_once('connect.php');
 
-// si pas d'id dans l'URL, on arrete
-/*if(!isset($_GET['id'])){
-    die('L\'id pas spécifiée!');
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $sql = "INSERT INTO `quizz`(`id_cours`, `mot`, `video1`, `video2`, `video3`, `reussi`)
+    VALUES (:id_cours, :mot, :video1, :video2, :video3, NULL)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id_cours', $_POST['id_cours'], PDO::PARAM_INT);
+    $stmt->bindValue(':mot', $_POST['mot'], PDO::PARAM_STR);
+    $stmt->bindValue(':video1', $_FILES['video1']['name']);
+    $stmt->bindValue(':video2', $_FILES['video2']['name']);
+    $stmt->bindValue(':video3', $_FILES['video3']['name']);
+    $stmt->execute();
+    //header('Location: back_cours.php');
+var_dump($_POST);
 }
+
+	if(!empty($_POST['submit']) && isset($_FILES['video1']))
+     {
+
+         $destination = "../video/";
+
+         $filename = basename($_FILES['video1']['name']);
+
+         move_uploaded_file($_FILES['video1']['tmp_name'], $destination . $filename);
+         //header('Location: back_cours.php');
+
+   }
+    if(!empty($_POST['submit']) && isset($_FILES['video2']))
+    {
+
+        $destination = "../video/";
+
+        $filename = basename($_FILES['video2']['name']);
+
+        move_uploaded_file($_FILES['video2']['tmp_name'], $destination . $filename);
+        //header('Location: back_cours.php');
+
+    }
+    if(!empty($_POST['submit']) && isset($_FILES['video3']))
+    {
+
+        $destination = "../video/";
+
+        $filename = basename($_FILES['video3']['name']);
+
+        move_uploaded_file($_FILES['video3']['tmp_name'], $destination . $filename);
+        header('Location: back_cours.php');
+
+    }
+
+// si pas d'id dans l'URL, on arrete
+//if(!isset($_GET['id'])){
+  //  die('L\'id pas spécifiée!');
+//}
 // requete SQL
-$sql = "SELECT `id_cours` FROM `cours` WHERE id_cours = :id";
+
+$sql = "SELECT id_cours FROM cours WHERE id_cours = :id";
 // prepare
 $stmt = $pdo->prepare($sql);
 // bind
@@ -33,33 +83,6 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if(false === $row){
     die('L\'id n\'existe pas!');
 }
-*/
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-    $sql = "INSERT INTO `quizz`(`id_cours`, `question`, `video1`, `video2`, `video3`, `reussi`)
-    VALUES (:id_cours, :question, :video1, :video2, :video3, NULL)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':id_cours', $_POST['id_cours'], PDO::PARAM_INT);
-    $stmt->bindValue(':question', $_POST['question'], PDO::PARAM_STR);
-    $stmt->bindValue(':video1', $_FILES['video1']['name']);
-    $stmt->bindValue(':video2', $_FILES['video2']['name']);
-    $stmt->bindValue(':video3', $_FILES['video3']['name']);
-    $stmt->execute();
-
-}
-
-	if(!empty($_POST['submit']) && isset($_FILES['video']))
-     {
-
-         $destination = "../video/";
-
-         $filename = basename($_FILES['video']['name']);
-
-         move_uploaded_file($_FILES['video']['tmp_name'], $destination . $filename);
-         //header('Location: back_cours.php');
-
-   }
-
 //include('../include/header_back.php');?>
 
 <section class="white">
@@ -70,28 +93,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         <form method="post" enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>">
 
-            <div class="row hidden">
+            <div class="row">
                 <label for="id_cours" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">id_cours</label>
-                <input type="text" name="id_cours" title="id_cours" value="1"  class="browser-default col s10 offset-s1 m7 l7">
+                <input type="text" name="id_cours" title="id_cours" value="<?=$row['id_cours']?>"  class="browser-default col s10 offset-s1 m7 l7">
             </div>
 
             <div class="row">
-                <label for="question" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Question</label>
-                <input type="text" name="question" title="question" class="browser-default col s10 offset-s1 m7 l7">
+                <label for="mot" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Mot</label>
+                <input type="text" name="mot" title="mot" class="browser-default col s10 offset-s1 m7 l7">
             </div>
 
             <div class="row">
-                <label for="file" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz</label>
+                <label for="video1" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz 1</label>
                 <input type="file" title="video1" name="video1" class="browser-default col s10 offset-s1 m7 l7"/>
             </div>
 
             <div class="row">
-                <label for="file" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz</label>
+                <label for="video2" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz 2</label>
                 <input type="file" title="video2" name="video2" class="browser-default col s10 offset-s1 m7 l7"/>
             </div>
 
             <div class="row">
-                <label for="file" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz</label>
+                <label for="video3" class="browser-default col offset-m1 m3 offset-l1 l3 s10 offset-s1 p-10">Vidéo des Quizz 3</label>
                 <input type="file" title="video3" name="video3" class="browser-default col s10 offset-s1 m7 l7"/>
             </div>
 
