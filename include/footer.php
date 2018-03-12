47e8e51238903e97040ee62b3e17c685658ffaec
Zerby <clement.zerbi@eemi.com>
2018-03-12 23:14:49 +0100

    footer v2

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,8 @@
 </main>
 <footer class="blue">
     <div class="row">
-        <div class="col s6 push-s5"><img src="../images/logo.png" alt="logo" class="w-30"></div>
-        <div class="col s6 push-s3 padding-2"><img src="../images/w3c-logo.png" alt="w3c" class="w-15"></div>
+        <div class="col s6 m6 push-m5 l6 push-l5"><img src="../images/logo.png" class="logo_footer" alt="logo"></div>
+        <div class="col s6 m5 l1 offset-l3 padding-2 right-align"><img src="../images/w3c-logo.png" class="logo_w3c" alt="w3c"></div>
     </div>
     <div class="row">
         <div class="col s12 m7 l7">
@@ -27,9 +27,9 @@
         </div>
         <div class="col s10 m4 push-m1 l4 push-l1">
             <div class="row">
-                <div class="col s4"><a><img src="../images/facebook.png" class="w-35"></a></div>
-                <div class="col s4"><a><img src="../images/instagram.png" class="w-35"></a></div>
-                <div class="col s4"><a><img src="../images/twitter.png" class="w-35"></a></div>
+                <div class="col s4"><a><img src="../images/facebook.png" alt="facebook" class="rs_logo"></a></div>
+                <div class="col s4"><a><img src="../images/instagram.png" alt="instagram" class="rs_logo"></a></div>
+                <div class="col s4"><a><img src="../images/twitter.png" alt="twitter" class="rs_logo"></a></div>
             </div>
         </div>
     </div>
